0461117f49eb78761b5c0cfce21ace0099ebfef7
Manuel Schmid <manysch3@gmail.com>
2025-01-02 18:33:50 +0100

    outsource paths to separate file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/Solar-System-Simulation/',
+    base: '/Solar-System-Simulation/', // /Solar-System-Simulation/
 });
